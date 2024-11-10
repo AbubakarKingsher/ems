6fc8ecf90da936681f9bfc261273a57f71e8561f
AbubakarKingsher <abubakarmarri007@gmail.com>
2024-11-10 20:40:55 +0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Employee-Management-System/',
+  base: '/ems/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-  },
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
