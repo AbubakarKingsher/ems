d82a2866af23776c9bd81694d4c8b73bc81dbec0
AbubakarKingsher <abubakarmarri007@gmail.com>
2024-11-14 20:22:04 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./components/Auth/Login";
 import EmployeDashboard from "./components/Dashboard/EmployeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import { AuthContext } from "./context/AuthProvider";
+import { HashRouter } from 'react-router-dom'
 
 function App() {
   const [user, setUser] = useState(null);
@@ -41,14 +42,14 @@ function App() {
   };
 
   return (
-    <>
+    <HashRouter>
       {!user ? <Login handleLogin={handleLogin} /> : ""}
       {user == "admin" ? (
         <AdminDashboard changeUser={setUser} />
       ) : user == "employee" ? (
         <EmployeDashboard changeUser={setUser} data={loggedInUserData} />
       ) : null}
-    </>
+    </HashRouter>
   );
 }
 
